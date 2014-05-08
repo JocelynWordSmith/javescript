96e7b5b57763d45f8a250ed3152241e685cad4aa
joshuawordsmith <Joshua Smith>
2014-05-08 00:41:02 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,248 @@
+
+var name = "Joshua";
+
+name = name.toLowerCase();
+
+console.log(name);
+
+var names = new Array(name.split(''));
+
+console.log(names);
+
+// (function()){
+// 	namechop(function(){return (name.split(2, name.length - 2);
+// 		document.write(name);})()
+// }
+// name.namechop = function () {
+// 	return (name.split(2, name.length - 2);
+// 		document.write(name);
+// }
+////////////////////////////////
+////////////////////////////////
+//date, found online to play with
+var now = new Date();
+
+var days = new Array('Sunday','Morndas','Tuesday','Wednesday','Thursday','Friday','Saturday');
+
+var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+
+var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
+
+function fourdigits(number)	{
+	return (number < 1000) ? number + 1900 : number;
+								}
+today =  days[now.getDay()] + ", " +
+         months[now.getMonth()] + " " +
+         date + ", " +
+         (fourdigits(now.getYear())) ;
+
+document.write(today);
+//////////////////////////////////
+//////////////////////////////////
+//cal, found online to play with
+
+
+function maxDays(mm, yyyy){
+var mDay;
+	if((mm == 3) || (mm == 5) || (mm == 8) || (mm == 10)){
+		mDay = 30;
+  	}
+  	else{
+  		mDay = 31
+  		if(mm == 1){
+   			if (yyyy/4 - parseInt(yyyy/4) != 0){
+   				mDay = 28
+   			}
+		   	else{
+   				mDay = 29
+  			}
+		}
+  }
+return mDay;
+}
+function changeBg(id){
+	if (eval(id).style.backgroundColor != "yellow"){
+		eval(id).style.backgroundColor = "yellow"
+	}
+	else{
+		eval(id).style.backgroundColor = "#ffffff"
+	}
+}
+function writeCalendar(){
+var now = new Date
+var dd = now.getDate()
+var mm = now.getMonth()
+var dow = now.getDay()
+var yyyy = now.getFullYear()
+var arrM = new Array("January","February","March","April","May","June","July","August","September","October","November","December")
+var arrY = new Array()
+	for (ii=0;ii<=4;ii++){
+		arrY[ii] = yyyy - 2 + ii
+	}
+var arrD = new Array("Sun","Mon","Tue","Wed","Thu","Fri","Sat")
+
+var text = ""
+text = "<form name=calForm>"
+text += "<table border=1>"
+text += "<tr><td>"
+text += "<table width=100%><tr>"
+text += "<td align=left>"
+text += "<select name=selMonth onChange='changeCal()'>"
+	for (ii=0;ii<=11;ii++){
+		if (ii==mm){
+			text += "<option value= " + ii + " Selected>" + arrM[ii] + "</option>"
+		}
+		else{
+			text += "<option value= " + ii + ">" + arrM[ii] + "</option>"
+		}
+	}
+text += "</select>"
+text += "</td>"
+text += "<td align=right>"
+text += "<select name=selYear onChange='changeCal()'>"
+	for (ii=0;ii<=4;ii++){
+		if (ii==2){
+			text += "<option value= " + arrY[ii] + " Selected>" + arrY[ii] + "</option>"
+		}
+		else{
+			text += "<option value= " + arrY[ii] + ">" + arrY[ii] + "</option>"
+		}
+	}
+text += "</select>"
+text += "</td>"
+text += "</tr></table>"
+text += "</td></tr>"
+text += "<tr><td>"
+text += "<table border=1>"
+text += "<tr>"
+	for (ii=0;ii<=6;ii++){
+		text += "<td align=center><span class=label>" + arrD[ii] + "</span></td>"
+	}
+text += "</tr>"
+aa = 0
+	for (kk=0;kk<=5;kk++){
+		text += "<tr>"
+		for (ii=0;ii<=6;ii++){
+			text += "<td align=center><span id=sp" + aa + " onClick='changeBg(this.id)'>1</span></td>"
+			aa += 1
+		}
+		text += "</tr>"
+	}
+text += "</table>"
+text += "</td></tr>"
+text += "</table>"
+text += "</form>"
+document.write(text)
+changeCal()
+}
+function changeCal(){
+var now = new Date
+var dd = now.getDate()
+var mm = now.getMonth()
+var dow = now.getDay()
+var yyyy = now.getFullYear()
+var currM = parseInt(document.calForm.selMonth.value)
+var prevM
+	if (currM!=0){
+		prevM = currM - 1
+	}
+	else{
+		prevM = 11
+	}
+var currY = parseInt(document.calForm.selYear.value)
+var mmyyyy = new Date()
+mmyyyy.setFullYear(currY)
+mmyyyy.setMonth(currM)
+mmyyyy.setDate(1)
+var day1 = mmyyyy.getDay()
+	if (day1 == 0){
+		day1 = 7
+	}
+var arrN = new Array(41)
+var aa
+	for (ii=0;ii<day1;ii++){
+		arrN[ii] = maxDays((prevM),currY) - day1 + ii + 1
+	}
+	aa = 1
+	for (ii=day1;ii<=day1+maxDays(currM,currY)-1;ii++){
+		arrN[ii] = aa
+		aa += 1
+	}
+	aa = 1
+	for (ii=day1+maxDays(currM,currY);ii<=41;ii++){
+		arrN[ii] = aa
+		aa += 1
+	}
+	for (ii=0;ii<=41;ii++){
+		eval("sp"+ii).style.backgroundColor = "#FFFFFF"
+	}
+var dCount = 0
+	for (ii=0;ii<=41;ii++){
+		if (((ii<7)&&(arrN[ii]>20))||((ii>27)&&(arrN[ii]<20))){
+			eval("sp"+ii).innerHTML = arrN[ii]
+			eval("sp"+ii).className = "c3"
+		}
+		else{
+			eval("sp"+ii).innerHTML = arrN[ii]
+			if ((dCount==0)||(dCount==6)){
+				eval("sp"+ii).className = "c2"
+			}
+			else{
+				eval("sp"+ii).className = "c1"
+			}
+			if ((arrN[ii]==dd)&&(mm==currM)&&(yyyy==currY)){
+				eval("sp"+ii).style.backgroundColor="#90EE90"
+			}
+		}
+	dCount += 1
+		if (dCount>6){
+			dCount=0
+		}
+	}
+}
+///////////////////////////////
+///////////////////////////////
+//IP Address
+// var ip = '<!--#echo var="REMOTE_ADDR"-->';
+// document.write("Your IP address is" + ip);
+
+var charCodes=new Array(36); 
+charCodes["a"]=". _";
+charCodes["b"]="_ . . .";
+charCodes["c"]="_ . _ .";
+charCodes["d"]="_ . .";
+charCodes["e"]=".";
+charCodes["f"]=". . _ .";
+charCodes["g"]="_ _ .";
+charCodes["h"]=". . . .";
+charCodes["i"]=". .";
+charCodes["j"]=". _ _ _";
+charCodes["k"]="_ . _";
+charCodes["l"]=". _ . .";
+charCodes["m"]="_ _";
+charCodes["n"]="_ .";
+charCodes["o"]="_ _ _";
+charCodes["p"]=". _ _ .";
+charCodes["q"]="_ _ . _";
+charCodes["r"]=". _ .";
+charCodes["s"]=". . .";
+charCodes["t"]="_";
+charCodes["u"]=". . _";
+charCodes["v"]=". . . _";
+charCodes["w"]=". _ _";
+charCodes["x"]="_ . . _";
+charCodes["y"]="_ . _ _";
+charCodes["z"]="_ _ . .";
+charCodes["1"]=". _ _ _ _";
+charCodes["2"]=". . _ _ _";
+charCodes["3"]=". . . _ _";
+charCodes["4"]=". . . . _";
+charCodes["5"]=". . . . .";
+charCodes["6"]="_ . . . .";
+charCodes["7"]="_ _ . . .";
+charCodes["8"]="_ _ _ . .";
+charCodes["9"]="_ _ _ _ .";
+charCodes["0"]="_ _ _ _ _";
+var temp=''
+
+document.write(names)
